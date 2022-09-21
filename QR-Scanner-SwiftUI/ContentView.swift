733688dec90516903c8710c15b97d7f928667294
jonasbergstrom <jontrian97@gmail.com>
2022-09-21 12:41:08 +0200

    Initial Commit

---
+++ QR-Scanner-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QR-Scanner-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-09-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QR-Scanner-SwiftUI/QR_Scanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_Scanner_SwiftUIApp.swift
+//  QR-Scanner-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-09-21.
+//
+
+import SwiftUI
+
+@main
+struct QR_Scanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
